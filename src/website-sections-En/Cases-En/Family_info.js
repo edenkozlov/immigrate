f7f6ed04347050400287e8647ff423cd97c51a52
edenkozlov <96761592+edenkozlov@users.noreply.github.com>
2023-06-13 20:54:45 -0400

    PI

--- src/website-sections-En/Cases-En/Family_info.js
+++ src/website-sections-En/Cases-En/Family_info.js
@@ -1,7 +1,8 @@
 import MyStyling from "./case-input-styling";
 import DropdownStyle from "./case-dropdown-style";
+import React from "react";
 
-function renderInput(id, label, value, maxLength, placeholder) {
+function renderInput(id, label, value, maxLength, placeholder, handleChange) {
   return (
     <div>
       <label htmlFor={`input-${id}`}>{label}</label>
@@ -11,20 +12,17 @@ function renderInput(id, label, value, maxLength, placeholder) {
         value={value}
         maxLength={maxLength}
         placeholder={placeholder}
+        onChange={handleChange}
       />
     </div>
   );
 }
 
-function renderDropdown(id, label, value, options, placeholder) {
+function renderDropdown(id, label, value, options, placeholder, handleChange) {
   return (
     <div>
       <label htmlFor={`dropdown-${id}`}>{label}</label>
-      <select
-        id={`dropdown-${id}`}
-        value={value}
-        style={DropdownStyle}
-      >
+      <select id={`dropdown-${id}`} value={value} style={DropdownStyle} onChange={handleChange}>
         <option value="" disabled>
           {placeholder}
         </option>
@@ -39,14 +37,320 @@ function renderDropdown(id, label, value, options, placeholder) {
 }
 
 function FamilyInfo() {
+  const [maritalStatusValue, setMaritalStatusValue] = React.useState('');
+  const [previouslyMarriedValue, setPreviouslyMarriedValue] = React.useState('');
+  const [input24Value, setInput24Value] = React.useState('');
+  const [input25Value, setInput25Value] = React.useState('');
+  const [input26Value, setInput26Value] = React.useState('');
+  const [input27Value, setInput27Value] = React.useState('');
+  const [input28Value, setInput28Value] = React.useState('');
+  const [input29Value, setInput29Value] = React.useState('');
+  const [input30Value, setInput30Value] = React.useState('');
+  const [input31Value, setInput31Value] = React.useState('');
+  const [input32Value, setInput32Value] = React.useState('');
+  const [input34Value, setInput34Value] = React.useState('');
+  const [input35Value, setInput35Value] = React.useState('');
+  const [input36Value, setInput36Value] = React.useState('');
+  const [input37Value, setInput37Value] = React.useState('');
+  const [input38Value, setInput38Value] = React.useState('');
+  const [input39Value, setInput39Value] = React.useState('');
+
+  const handleMaritalStatusChange = (e) => {
+    setMaritalStatusValue(e.target.value);
+  };
+
+  const handlePreviouslyMarriedChange = (e) => {
+    setPreviouslyMarriedValue(e.target.value);
+  };
+
+  const handleInputChange24 = (e) => {
+    setInput24Value(e.target.value);
+  };
+
+  const handleInputChange25 = (e) => {
+    setInput25Value(e.target.value);
+  };
+
+  const handleInputChange26 = (e) => {
+    setInput26Value(e.target.value);
+  };
+
+  const handleInputChange27 = (e) => {
+    setInput27Value(e.target.value);
+  };
+
+  const handleInputChange28 = (e) => {
+    setInput28Value(e.target.value);
+  };
+
+  const handleInputChange29 = (e) => {
+    setInput29Value(e.target.value);
+  };
+
+  const handleInputChange30 = (e) => {
+    setInput30Value(e.target.value);
+  };
+
+  const handleInputChange31 = (e) => {
+    setInput31Value(e.target.value);
+  };
+
+  const handleInputChange32 = (e) => {
+    setInput32Value(e.target.value);
+  };
+
+  const handleInputChange34 = (e) => {
+    setInput34Value(e.target.value);
+  };
+
+  const handleInputChange35 = (e) => {
+    setInput35Value(e.target.value);
+  };
+
+  const handleInputChange36 = (e) => {
+    setInput36Value(e.target.value);
+  };
+
+  const handleInputChange37 = (e) => {
+    setInput37Value(e.target.value);
+  };
+
+  const handleInputChange38 = (e) => {
+    setInput38Value(e.target.value);
+  };
+
+  const handleInputChange39 = (e) => {
+    setInput39Value(e.target.value);
+  };
+
   return (
     <div>
       <MyStyling />
       <div className="myrowfr">
-        {renderInput(0, 'test', '', 20, 'Example: test')}
-        {renderInput(1, 'Your given name(s) (as shown on passport)', '', 20, 'Example: John')}
-        {renderInput(2, 'Your date of birth: (YYYY/MM/DD)', '', 10, 'Example: 1985/01/01')}
+        {renderDropdown(
+          22,
+          'Current marital status (required)',
+          maritalStatusValue,
+          ['Annulled Marriage', 'Common-Law', 'Divorced', 'Married', 'Separated', 'Single', 'Unknown', 'Widowed'],
+          'Example: Select an option',
+          handleMaritalStatusChange
+        )}
       </div>
+      {['Annulled Marriage', 'Divorced', 'Separated', 'Widowed'].includes(maritalStatusValue.trim()) && (
+        <div className="myrowfr">
+          {renderDropdown(
+            23,
+            'Have you previously been married or in a common-law relationship? (required)',
+            previouslyMarriedValue,
+            ['Yes'],
+            'Example: Select an option',
+            handlePreviouslyMarriedChange
+          )}
+          {renderInput(
+            24,
+            'Enter your family or name(s) below (required field)',
+            input24Value,
+            20,
+            'Example: Smith',
+            handleInputChange24
+          )}
+          {renderInput(
+            25,
+            'Enter your given or name(s) below (required field)',
+            input25Value,
+            20,
+            'Example: John',
+            handleInputChange25
+          )}
+          {renderInput(
+            26,
+            'Date of birth (YYYY/MM/DD) (required)',
+            input26Value,
+            10,
+            'Example: 1985/01/01',
+            handleInputChange26
+          )}
+          {renderInput(
+            27,
+            'Type of relationship (required)',
+            input27Value,
+            20,
+            'Example: Spouse',
+            handleInputChange27
+          )}
+          {renderInput(
+            28,
+            'rel. start date(YYYY/MM/DD) (required)',
+            input28Value,
+            10,
+            'Example: 2010/01/01',
+            handleInputChange28
+          )}
+          {renderInput(
+            29,
+            'End date of relationship (YYYY/MM/DD) (required)',
+            input29Value,
+            10,
+            'Example: 2020/12/31',
+            handleInputChange29
+          )}
+        </div>
+      )}
+      {['Married', 'Common-Law'].includes(maritalStatusValue.trim()) && (
+        <div>
+          <h5>(If you are married or in a common-law relationship) Provide the date on which you were married or entered into the common-law relationship. In addition, please provide the name of your current/spouse/common-law partner.</h5>
+          <div className="myrowfr">
+            {renderInput(
+              30,
+              'Date (YYYY/MM/DD) (required)',
+              input30Value,
+              10,
+              'Example: 2022/01/01',
+              handleInputChange30
+            )}
+            {renderInput(
+              31,
+              'Family name(s) (required)',
+              input31Value,
+              20,
+              'Example: Smith',
+              handleInputChange31
+            )}
+            {renderInput(
+              32,
+              'Given name(s)',
+              input32Value,
+              20,
+              'Example: John',
+              handleInputChange32
+            )}
+            {renderDropdown(
+            33,
+            'Have you previously been married or in a common-law relationship? (required)',
+            previouslyMarriedValue,
+            ['Yes', 'No'],
+            'Example: Select an option',
+            handlePreviouslyMarriedChange
+          )}
+          {previouslyMarriedValue === 'Yes' && (
+            <>
+              {renderInput(
+                34,
+                'Enter your family or name(s) below (required field)',
+                input34Value,
+                20,
+                'Example: Smith',
+                handleInputChange34
+              )}
+              {renderInput(
+                35,
+                'Enter your given or name(s) below (required field)',
+                input35Value,
+                20,
+                'Example: John',
+                handleInputChange35
+              )}
+              {renderInput(
+                36,
+                'Date of birth (YYYY/MM/DD) (required)',
+                input36Value,
+                10,
+                'Example: 1985/01/01',
+                handleInputChange36
+              )}
+              {renderInput(
+                37,
+                'Type of relationship (required)',
+                input37Value,
+                20,
+                'Example: Spouse',
+                handleInputChange37
+              )}
+              {renderInput(
+                38,
+                'rel. start date(YYYY/MM/DD) (required)',
+                input38Value,
+                10,
+                'Example: 2010/01/01',
+                handleInputChange38
+              )}
+              {renderInput(
+                39,
+                'End date of relationship (YYYY/MM/DD) (required)',
+                input39Value,
+                10,
+                'Example: 2020/12/31',
+                handleInputChange39
+              )}
+            </>
+          )}
+          </div>
+        </div>
+      )}
+      {['Single', 'Unknown'].includes(maritalStatusValue.trim()) && (
+        <div className="myrowfr">
+          {renderDropdown(
+            33,
+            'Have you previously been married or in a common-law relationship? (required)',
+            previouslyMarriedValue,
+            ['Yes', 'No'],
+            'Example: Select an option',
+            handlePreviouslyMarriedChange
+          )}
+          {previouslyMarriedValue === 'Yes' && (
+            <>
+              {renderInput(
+                34,
+                'Enter your family or name(s) below (required field)',
+                input34Value,
+                20,
+                'Example: Smith',
+                handleInputChange34
+              )}
+              {renderInput(
+                35,
+                'Enter your given or name(s) below (required field)',
+                input35Value,
+                20,
+                'Example: John',
+                handleInputChange35
+              )}
+              {renderInput(
+                36,
+                'Date of birth (YYYY/MM/DD) (required)',
+                input36Value,
+                10,
+                'Example: 1985/01/01',
+                handleInputChange36
+              )}
+              {renderInput(
+                37,
+                'Type of relationship (required)',
+                input37Value,
+                20,
+                'Example: Spouse',
+                handleInputChange37
+              )}
+              {renderInput(
+                38,
+                'rel. start date(YYYY/MM/DD) (required)',
+                input38Value,
+                10,
+                'Example: 2010/01/01',
+                handleInputChange38
+              )}
+              {renderInput(
+                39,
+                'End date of relationship (YYYY/MM/DD) (required)',
+                input39Value,
+                10,
+                'Example: 2020/12/31',
+                handleInputChange39
+              )}
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- src/website-sections-En/Cases-En/Personal_info.js
+++ src/website-sections-En/Cases-En/Personal_info.js
@@ -41,9 +41,8 @@ function renderDropdown(id, label, value, options, placeholder, handleChange) {
   );
 }
 
-
 function Divider() {
-  return <hr style={{ borderTop: "2px solid #7D85C7", marginBottom: "20px" }} />;
+  return <hr style={{ borderTop: '2px solid #7D85C7', marginBottom: '20px' }} />;
 }
 
 function PersonalInfo() {
@@ -64,9 +63,30 @@ function PersonalInfo() {
   const [input15Value, setInput15Value] = React.useState('');
   const [input16Value, setInput16Value] = React.useState('');
   const [input17Value, setInput17Value] = React.useState('');
+  const [maritalStatusValue, setMaritalStatusValue] = React.useState('');
+  const [previouslyMarriedValue, setPreviouslyMarriedValue] = React.useState('');
+  const [input24Value, setInput24Value] = React.useState('');
+  const [input25Value, setInput25Value] = React.useState('');
+  const [input26Value, setInput26Value] = React.useState('');
+  const [input27Value, setInput27Value] = React.useState('');
+  const [input28Value, setInput28Value] = React.useState('');
+  const [input29Value, setInput29Value] = React.useState('');
+  const [input30Value, setInput30Value] = React.useState('');
+  const [input31Value, setInput31Value] = React.useState('');
+  const [input32Value, setInput32Value] = React.useState('');
+  const [input34Value, setInput34Value] = React.useState('');
+  const [input35Value, setInput35Value] = React.useState('');
+  const [input36Value, setInput36Value] = React.useState('');
+  const [input37Value, setInput37Value] = React.useState('');
+  const [input38Value, setInput38Value] = React.useState('');
+  const [input39Value, setInput39Value] = React.useState('');
+
+
+  
   const [hadPreviousCountries, setHadPreviousCountries] = React.useState('');
   const [previousCountries, setPreviousCountries] = React.useState([]);
-  const [maritalStatusValue, setMaritalStatusValue] = React.useState('');
+  
+
 
   const handleInputChange1 = (e) => setInput1Value(e.target.value);
   const handleInputChange2 = (e) => setInput2Value(e.target.value);
@@ -88,15 +108,20 @@ function PersonalInfo() {
 
   
 
+
   const handlePreviousCountriesChange = (e) => setHadPreviousCountries(e.target.value);
-  const handleMaritalStatusChange = (e) => {setMaritalStatusValue(e.target.value);};
+  const handleMaritalStatusChange = (e) => {
+    setMaritalStatusValue(e.target.value);
+  };
+
+  
 
   const handleAddCountry = () => {
     const newCountry = {
       country: '',
       immigrationStatus: '',
       startDate: '',
-      endDate: ''
+      endDate: '',
     };
     setPreviousCountries([...previousCountries, newCountry]);
   };
@@ -129,6 +154,70 @@ function PersonalInfo() {
     const updatedCountries = [...previousCountries];
     updatedCountries[index].endDate = value;
     setPreviousCountries(updatedCountries);
+  };
+
+  const handlePreviouslyMarriedChange = (e) => {
+    setPreviouslyMarriedValue(e.target.value);
+  };
+
+  const handleInputChange24 = (e) => {
+    setInput24Value(e.target.value);
+  };
+
+  const handleInputChange25 = (e) => {
+    setInput25Value(e.target.value);
+  };
+
+  const handleInputChange26 = (e) => {
+    setInput26Value(e.target.value);
+  };
+
+  const handleInputChange27 = (e) => {
+    setInput27Value(e.target.value);
+  };
+
+  const handleInputChange28 = (e) => {
+    setInput28Value(e.target.value);
+  };
+
+  const handleInputChange29 = (e) => {
+    setInput29Value(e.target.value);
+  };
+
+  const handleInputChange30 = (e) => {
+    setInput30Value(e.target.value);
+  };
+
+  const handleInputChange31 = (e) => {
+    setInput31Value(e.target.value);
+  };
+
+  const handleInputChange32 = (e) => {
+    setInput32Value(e.target.value);
+  };
+
+  const handleInputChange34 = (e) => {
+    setInput34Value(e.target.value);
+  };
+
+  const handleInputChange35 = (e) => {
+    setInput35Value(e.target.value);
+  };
+
+  const handleInputChange36 = (e) => {
+    setInput36Value(e.target.value);
+  };
+
+  const handleInputChange37 = (e) => {
+    setInput37Value(e.target.value);
+  };
+
+  const handleInputChange38 = (e) => {
+    setInput38Value(e.target.value);
+  };
+
+  const handleInputChange39 = (e) => {
+    setInput39Value(e.target.value);
   };
 
   return (
@@ -170,11 +259,18 @@ function PersonalInfo() {
           6,
           'Your UCI number (no UCI num? - type "xx")',
           input7Value,
-           10,
+          10,
           'Example: 1234567890',
           handleInputChange7
         )}
-        {renderDropdown(7, 'Sex', input8Value, ['Male', 'Female', 'Unknown', 'Another Gender'], 'Example: Select an option', handleInputChange8)}
+        {renderDropdown(
+          7,
+          'Sex',
+          input8Value,
+          ['Male', 'Female', 'Unknown', 'Another Gender'],
+          'Example: Select an option',
+          handleInputChange8
+        )}
         {renderDropdown(
           8,
           'Eye colour',
@@ -225,7 +321,7 @@ function PersonalInfo() {
       <div className="myrowfr">
         {renderDropdown(
           15,
-          'country of residence - Status (required)',
+          'Country of residence - Status (required)',
           input16Value,
           ['Citizen', 'Permanent Resident', 'Visitor', 'Worker'],
           'Example: Select an option',
@@ -252,57 +348,52 @@ function PersonalInfo() {
           handlePreviousCountriesChange
         )}
       </div>
-      
+
       {hadPreviousCountries === 'Yes' && (
         <div>
-          
-          
           <h5>Previous countries of residence:</h5>
           {previousCountries.map((country, index) => (
             <div>
-            <div key={index} className="myrowfr">
-              {renderDropdown(
-                18,
-                `Previous country of residence #${index + 1}`,
-                country.country,
-                ['Country 1', 'Country 2', 'Country 3', 'Country 4'],
-                'Example: Select an option',
-                (e) => handleCountryChange(index, e.target.value)
-              )}
-              {renderDropdown(
-                19,
-                'Immigration status',
-                country.immigrationStatus,
-                ['Status 1', 'Status 2', 'Status 3', 'Status 4'],
-                'Example: Select an option',
-                (e) => handleImmigrationStatusChange(index, e.target.value)
-              )}
-              {renderInput(
-                20,
-                'Start date of immigration status (YYYY/MM/DD)',
-                country.startDate,
-                10,
-                'Example: 2022/01/01',
-                (e) => handleStartDateChange(index, e.target.value)
-              )}
-              {renderInput(
-                21,
-                'End date of immigration status (YYYY/MM/DD)',
-                country.endDate,
-                10,
-                'Example: 2022/12/31',
-                (e) => handleEndDateChange(index, e.target.value)
-              )}
-
+              <div key={index} className="myrowfr">
+                {renderDropdown(
+                  18,
+                  `Previous country of residence #${index + 1}`,
+                  country.country,
+                  ['Country 1', 'Country 2', 'Country 3', 'Country 4'],
+                  'Example: Select an option',
+                  (e) => handleCountryChange(index, e.target.value)
+                )}
+                {renderDropdown(
+                  19,
+                  'Immigration status',
+                  country.immigrationStatus,
+                  ['Status 1', 'Status 2', 'Status 3', 'Status 4'],
+                  'Example: Select an option',
+                  (e) => handleImmigrationStatusChange(index, e.target.value)
+                )}
+                {renderInput(
+                  20,
+                  'Start date of immigration status (YYYY/MM/DD)',
+                  country.startDate,
+                  10,
+                  'Example: 2022/01/01',
+                  (e) => handleStartDateChange(index, e.target.value)
+                )}
+                {renderInput(
+                  21,
+                  'End date of immigration status (YYYY/MM/DD)',
+                  country.endDate,
+                  10,
+                  'Example: 2022/12/31',
+                  (e) => handleEndDateChange(index, e.target.value)
+                )}
+              </div>
+              <button onClick={() => handleRemoveCountry(index)}>Remove</button>
             </div>
-            <button onClick={() => handleRemoveCountry(index)}>Remove</button>
-            </div>
-            
           ))}
-          
+
           <button onClick={handleAddCountry}>Add Another</button>
         </div>
-        
       )}
       <Divider />
       <div className="myrowfr">
@@ -310,11 +401,226 @@ function PersonalInfo() {
           22,
           'Current marital status (required)',
           maritalStatusValue,
-          [' Annulled Marriage ', ' Common-Law ', ' Divorced ', ' Married ', ' Separated ', '  Single  ', '  Unknown  ', '  Widowed  '],
+          ['Annulled Marriage', 'Common-Law', 'Divorced', 'Married', 'Separated', 'Single', 'Unknown', 'Widowed'],
           'Example: Select an option',
           handleMaritalStatusChange
         )}
       </div>
+      {['Annulled Marriage', 'Divorced', 'Separated', 'Widowed'].includes(maritalStatusValue.trim()) && (
+        <div className="myrowfr">
+          {renderDropdown(
+            23,
+            'Have you previously been married or in a common-law relationship? (required)',
+            previouslyMarriedValue,
+            ['Yes'],
+            'Example: Select an option',
+            handlePreviouslyMarriedChange
+          )}
+          {renderInput(
+            24,
+            'Enter your family or name(s) below (required field)',
+            input24Value,
+            20,
+            'Example: Smith',
+            handleInputChange24
+          )}
+          {renderInput(
+            25,
+            'Enter your given or name(s) below (required field)',
+            input25Value,
+            20,
+            'Example: John',
+            handleInputChange25
+          )}
+          {renderInput(
+            26,
+            'Date of birth (YYYY/MM/DD) (required)',
+            input26Value,
+            10,
+            'Example: 1985/01/01',
+            handleInputChange26
+          )}
+          {renderInput(
+            27,
+            'Type of relationship (required)',
+            input27Value,
+            20,
+            'Example: Spouse',
+            handleInputChange27
+          )}
+          {renderInput(
+            28,
+            'rel. start date(YYYY/MM/DD) (required)',
+            input28Value,
+            10,
+            'Example: 2010/01/01',
+            handleInputChange28
+          )}
+          {renderInput(
+            29,
+            'End date of relationship (YYYY/MM/DD) (required)',
+            input29Value,
+            10,
+            'Example: 2020/12/31',
+            handleInputChange29
+          )}
+        </div>
+      )}
+      {['Married', 'Common-Law'].includes(maritalStatusValue.trim()) && (
+        <div>
+          <h5>(If you are married or in a common-law relationship) Provide the date on which you were married or entered into the common-law relationship. In addition, please provide the name of your current/spouse/common-law partner.</h5>
+          <div className="myrowfr">
+            {renderInput(
+              30,
+              'Date (YYYY/MM/DD) (required)',
+              input30Value,
+              10,
+              'Example: 2022/01/01',
+              handleInputChange30
+            )}
+            {renderInput(
+              31,
+              'Family name(s) (required)',
+              input31Value,
+              20,
+              'Example: Smith',
+              handleInputChange31
+            )}
+            {renderInput(
+              32,
+              'Given name(s)',
+              input32Value,
+              20,
+              'Example: John',
+              handleInputChange32
+            )}
+            {renderDropdown(
+            33,
+            'Have you previously been married or in a common-law relationship? (required)',
+            previouslyMarriedValue,
+            ['Yes', 'No'],
+            'Example: Select an option',
+            handlePreviouslyMarriedChange
+          )}
+          {previouslyMarriedValue === 'Yes' && (
+            <>
+              {renderInput(
+                34,
+                'Enter your family or name(s) below (required field)',
+                input34Value,
+                20,
+                'Example: Smith',
+                handleInputChange34
+              )}
+              {renderInput(
+                35,
+                'Enter your given or name(s) below (required field)',
+                input35Value,
+                20,
+                'Example: John',
+                handleInputChange35
+              )}
+              {renderInput(
+                36,
+                'Date of birth (YYYY/MM/DD) (required)',
+                input36Value,
+                10,
+                'Example: 1985/01/01',
+                handleInputChange36
+              )}
+              {renderInput(
+                37,
+                'Type of relationship (required)',
+                input37Value,
+                20,
+                'Example: Spouse',
+                handleInputChange37
+              )}
+              {renderInput(
+                38,
+                'rel. start date(YYYY/MM/DD) (required)',
+                input38Value,
+                10,
+                'Example: 2010/01/01',
+                handleInputChange38
+              )}
+              {renderInput(
+                39,
+                'End date of relationship (YYYY/MM/DD) (required)',
+                input39Value,
+                10,
+                'Example: 2020/12/31',
+                handleInputChange39
+              )}
+            </>
+          )}
+          </div>
+        </div>
+      )}
+      {['Single', 'Unknown'].includes(maritalStatusValue.trim()) && (
+        <div className="myrowfr">
+          {renderDropdown(
+            33,
+            'Have you previously been married or in a common-law relationship? (required)',
+            previouslyMarriedValue,
+            ['Yes', 'No'],
+            'Example: Select an option',
+            handlePreviouslyMarriedChange
+          )}
+          {previouslyMarriedValue === 'Yes' && (
+            <>
+              {renderInput(
+                34,
+                'Enter your family or name(s) below (required field)',
+                input34Value,
+                20,
+                'Example: Smith',
+                handleInputChange34
+              )}
+              {renderInput(
+                35,
+                'Enter your given or name(s) below (required field)',
+                input35Value,
+                20,
+                'Example: John',
+                handleInputChange35
+              )}
+              {renderInput(
+                36,
+                'Date of birth (YYYY/MM/DD) (required)',
+                input36Value,
+                10,
+                'Example: 1985/01/01',
+                handleInputChange36
+              )}
+              {renderInput(
+                37,
+                'Type of relationship (required)',
+                input37Value,
+                20,
+                'Example: Spouse',
+                handleInputChange37
+              )}
+              {renderInput(
+                38,
+                'rel. start date(YYYY/MM/DD) (required)',
+                input38Value,
+                10,
+                'Example: 2010/01/01',
+                handleInputChange38
+              )}
+              {renderInput(
+                39,
+                'End date of relationship (YYYY/MM/DD) (required)',
+                input39Value,
+                10,
+                'Example: 2020/12/31',
+                handleInputChange39
+              )}
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 }
